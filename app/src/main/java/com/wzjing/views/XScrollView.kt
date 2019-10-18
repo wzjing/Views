8ea006f67144a04b989f84782e1c1cbe99f8fe08
wangzijing <wangzijing@miliantech.com>
2019-10-18 13:13:22 +0800

    修复layout_height和margin的问题

--- app/src/main/java/com/wzjing/views/XScrollView.kt
+++ app/src/main/java/com/wzjing/views/XScrollView.kt
@@ -2,11 +2,16 @@ package com.wzjing.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import android.widget.ScrollView
+import androidx.core.view.children
+import kotlin.math.max
 import kotlin.math.min
 
 class XScrollView(context: Context, attrs: AttributeSet? = null) : ScrollView(context, attrs) {
+
+    private val TAG = "XScrollView"
 
     var maxHeight: Int = -1
 
@@ -17,14 +22,30 @@ class XScrollView(context: Context, attrs: AttributeSet? = null) : ScrollView(co
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        val actualHeight =
+        val computeHeight =
             View.resolveSizeAndState(MeasureSpec.getSize(heightMeasureSpec), heightMeasureSpec, 0)
-        val actualWidth =
-            View.resolveSizeAndState(MeasureSpec.getSize(widthMeasureSpec), widthMeasureSpec, 0)
-        setMeasuredDimension(
-            actualWidth,
-            if (maxHeight > 0) min(maxHeight, actualHeight) else actualHeight
+
+        val child = getChildAt(0)
+        child.measure(widthMeasureSpec, heightMeasureSpec)
+        val childHeight = child.measuredHeight
+
+        val realHeight = max(
+            minimumHeight,
+            if (maxHeight > 0) min(maxHeight, childHeight) else computeHeight
+        )
+//        setMeasuredDimension(computeWidth, realHeight)
+        super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(realHeight, MeasureSpec.EXACTLY))
+        Log.d(
+            TAG,
+            "computeHeight: $computeHeight/${MeasureSpec.getMode(computeHeight)}, maxHeight: $maxHeight, minHeight: $minimumHeight childHeight: $childHeight"
         )
     }
+
+//    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+//        super.onLayout(changed, l, t, r, b)
+//        for (child in children) {
+//            val childParams = child.layoutParams as MarginLayoutParams
+//            child.layout(childParams.topMargin, childParams.marginStart, measuredWidth - (childParams.marginStart + childParams.marginEnd), measuredHeight - (childParams.topMargin + childParams.bottomMargin))
+//        }
+//    }
 }
